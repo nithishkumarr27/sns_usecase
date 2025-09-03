fb20ebb8f4b75f2b1722098688895fcc92922b2f
fazil-ihub <fazil.a.ihub@snsgroups.com>
2025-09-03 09:20:45 +0530

    agent-updated

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -35,8 +35,11 @@ export default function AppRoutes() {
      
 
         {/* Agent Details */}
-        <Route path=":category/:categoryId/:subcategoryId/:agentId" element={<AgentsDetails />} />
+        {/* <Route path=":category/:categoryId/:subcategoryId/:agentId" element={<AgentsDetails />} /> */}
+        {/* <Route path=":category/:categoryId/:agentId" element={<AgentsDetails />} /> */}
     </Route>
+    <Route path="/agent-workbench/:category/:categoryId/:subcategoryId/:agentId" element={<AgentsDetails />} />
+    <Route path="/agent-workbench/:category/:categoryId/:agentId" element={<AgentsDetails />} />
     </Routes>
   );
 }
